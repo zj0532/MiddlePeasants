1c35594135c71cfc3224b3263e30b74caa3ebff8
zj <zj5230@126.com>
2018-10-30 17:51:43 +0800

    完成首页banner特效

--- application/www/controller/NewsController.php
+++ application/www/controller/NewsController.php
@@ -10,10 +10,19 @@ namespace app\www\controller;
 
 
 use think\Controller;
+use app\admin\model\NewsModel;
 
 class NewsController extends Controller
 {
     public function news(){
+        try{
+            $new = new NewsModel();
+            $list = $new ->order('ns_intime desc')->select();
+            $this->assign('list',$list);
+            Log::write('测试日志信息，这是警告级别，并且实时写入','notice');
+        }catch (\Exception $e){
+            Log::write($e->getMessage(),'error');
+        }
         return $this->fetch('news');
     }
     //新闻内容
